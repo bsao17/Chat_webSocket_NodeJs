8c7fe9d0003c10313e18c43121ed7227f0fd8966
bsao17 <declic62@gmail.com>
2021-02-01 11:03:22 +0100

    add script socket.io in html file and initalize the socket event

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const { isObject } = require('util');
+
 const app = require ('express')();
 const http = require ('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message : ' + msg);
+    });
 });
 
 http.listen(2005, () => {
